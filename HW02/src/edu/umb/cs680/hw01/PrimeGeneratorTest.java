771320dbdf06f0a3f9f63ecc284c0f215c7efe9a
ashil30 <ashil.shah30@gmail.com>
2022-11-04 20:14:05 -0400

    added files according to requirement

---
+++ HW02/src/edu/umb/cs680/hw01/PrimeGeneratorTest.java
@@ -0,0 +1,55 @@
+package edu.umb.cs680.hw01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeGeneratorTest {
+
+    @org.junit.jupiter.api.Test
+    void getPrimes() {
+        PrimeGenerator gen = new PrimeGenerator(1, 10);
+        gen.generatePrimes();
+        Long[] expectedPrimes = {2L, 3L, 5L, 7L};
+        assertArrayEquals( expectedPrimes,
+                gen.getPrimes().toArray() );
+    }
+    @org.junit.jupiter.api.Test
+    void generatePrimes5To5() {
+        try {
+            PrimeGenerator pGenerator = new PrimeGenerator(5L, 5L);
+            pGenerator.generatePrimes();
+            pGenerator.getPrimes();
+            fail("Invalid range test fail");
+        } catch (RuntimeException ex) {
+            assertEquals("Wrong input values: from=5 to=5", ex.getMessage());
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void generatePrimesNegative() {
+        try {
+            PrimeGenerator pGenerator = new PrimeGenerator(-5L, 5L);
+            pGenerator.generatePrimes();
+            pGenerator.getPrimes();
+            fail("Invalid range test fail");
+        } catch (RuntimeException ex) {
+            assertEquals("Wrong input values: from=-5 to=5", ex.getMessage());
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void generatePrimesReverse() {
+        try {
+            PrimeGenerator pGenerator = new PrimeGenerator(15L, 1L);
+            pGenerator.generatePrimes();
+            pGenerator.getPrimes();
+            fail("Invalid range test fail");
+        } catch (RuntimeException ex) {
+            assertEquals("Wrong input values: from=15 to=1", ex.getMessage());
+        }
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
