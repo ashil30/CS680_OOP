c43c5f725d26ef7a00ae15809fbb283e3de7393e
ashil30 <ashil.shah30@gmail.com>
2022-12-20 00:33:27 -0500

    updated 08

--- HW08/test/src/edu/umb/cs680/hw08/FileTest.java
+++ HW08/test/src/edu/umb/cs680/hw08/FileTest.java
@@ -12,20 +12,19 @@ class FileTest {
 
 
 	Directory root = new Directory(null,"root",0, localTime);
-	Directory applications = new Directory(root,"applications",0, localTime);
+	Directory apps = new Directory(root,"apps",0, localTime);
 	Directory home = new Directory(root,"home",0, localTime);
-	Directory code = new Directory(home,"code",0, localTime);
-	Directory pics = new Directory(home,"pics",0, localTime);
+	Directory bin = new Directory(root,"bin",0, localTime);
+	Directory pictures = new Directory(home,"pictures",0, localTime);
 
-	File a = new File(applications,"a",1,localTime);
-	File b = new File(home,"b",1,localTime);
-	File c = new File(code,"c",1,localTime);
-	File d = new File(code,"d",1,localTime);
-	File e = new File(pics,"e",1,localTime);
-	File f = new File(pics,"f",1,localTime);
+	File a = new File(pictures,"a",1,localTime);
+	File b = new File(pictures,"b",1,localTime);
+	File c = new File(home,"c",1,localTime);
+	File y = new File(bin,"y",1,localTime);
+	File x = new File(apps,"e",1,localTime);
 
-	Link x = new Link(home, "x", 0, localTime, applications);
-	Link y = new Link(code, "y", 0, localTime, a);
+	Link d = new Link(root, "x", 0, localTime, pictures);
+	Link e = new Link(root, "y", 0, localTime, apps);
 
 	private String[] FileToStringArray(File f) {
 		String fileInfo[] = {Boolean.toString(f.isDirectory()), f.getName(), Integer.toString(f.getSize()), f.getCreationTime().toString(), f.getParent().getName()};
@@ -34,20 +33,20 @@ class FileTest {
 
 	@Test
 	public void verifyFileEqualityA() {
-		String[] expected={"false","a","1",localTime.toString(),"applications"};
+		String[] expected={"false","a","1",localTime.toString(),"pictures"};
 		assertArrayEquals(expected,FileToStringArray(a));
 	}
 
 	@Test
 	public void verifyFileEqualityB() {
-		String[] expected={"false","b","1",localTime.toString(),"home"};
+		String[] expected={"false","b","1",localTime.toString(),"pictures"};
 		assertArrayEquals(expected,FileToStringArray(b));
 	}
 
 	@Test
 	public void verifyFileEqualityF() {
-		String[] expected={"false","f","1",localTime.toString(),"pics"};
-		assertArrayEquals(expected,FileToStringArray(f));
+		String[] expected={"false","y","1",localTime.toString(),"bin"};
+		assertArrayEquals(expected,FileToStringArray(y));
 	}
 
 	@Test
@@ -57,7 +56,7 @@ class FileTest {
 		assertFalse(c.isDirectory());
 		assertFalse(d.isDirectory());
 		assertFalse(e.isDirectory());
-		assertFalse(f.isDirectory());
+		assertFalse(a.isDirectory());
 	}
 
 }
--- HW08/test/src/edu/umb/cs680/hw08/LinkTest.java
+++ HW08/test/src/edu/umb/cs680/hw08/LinkTest.java
@@ -13,20 +13,18 @@ class LinkTest {
 
 
 	Directory root = new Directory(null,"root",0, localTime);
-	Directory applications = new Directory(root,"applications",0, localTime);
+	Directory apps = new Directory(root,"apps",0, localTime);
 	Directory home = new Directory(root,"home",0, localTime);
-	Directory code = new Directory(home,"code",0, localTime);
-	Directory pics = new Directory(home,"pics",0, localTime);
+	Directory bin = new Directory(root,"bin",0, localTime);
+	Directory pictures = new Directory(home,"pictures",0, localTime);
 
-	File a = new File(applications,"a",1,localTime);
-	File b = new File(home,"b",1,localTime);
-	File c = new File(code,"c",1,localTime);
-	File d = new File(code,"d",1,localTime);
-	File e = new File(pics,"e",1,localTime);
-	File f = new File(pics,"f",1,localTime);
+	File a = new File(pictures,"a",1,localTime);
+	File b = new File(pictures,"b",1,localTime);
+	File c = new File(home,"c",1,localTime);
 
-	Link x = new Link(home, "x", 0, localTime, applications);
-	Link y = new Link(code, "y", 0, localTime, a);
+
+	Link d = new Link(root, "d", 0, localTime, pictures);
+	Link e = new Link(root, "e", 0, localTime, apps);
 
 	private String[] LinkToStringArray(Link l) {
 		Optional<Directory> optionalDirectory = Optional.ofNullable(l.getParent());
@@ -36,26 +34,26 @@ class LinkTest {
 
 	@Test
 	public void verifyLinksTest() {
-		assertFalse(x.isDirectory());
-		assertFalse(y.isDirectory());
+		assertFalse(d.isDirectory());
+		assertFalse(e.isDirectory());
 	}
 
 	@Test
 	public void verifyTargetEqualityY() {
-		String[] expected={"false","y","0",localTime.toString(),"a"};
-		assertArrayEquals(expected,LinkToStringArray(y));
+		String[] expected={"false","e","0",localTime.toString(),"apps"};
+		assertArrayEquals(expected,LinkToStringArray(e));
 	}
 
 	@Test
 	public void verifyTargetEqualityX() {
-		String[] expected={"false","x","0",localTime.toString(),"applications"};
-		assertArrayEquals(expected,LinkToStringArray(x));
+		String[] expected={"false","d","0",localTime.toString(),"pictures"};
+		assertArrayEquals(expected,LinkToStringArray(d));
 	}
 
 	@Test
 	public void verifyParentOfTarget() {
-		assertSame("root", x.getTarget().getParent().getName());
-		assertSame("applications", y.getTarget().getParent().getName());
+		assertSame("home", d.getTarget().getParent().getName());
+		assertSame("root", e.getTarget().getParent().getName());
 	}
 
 }
