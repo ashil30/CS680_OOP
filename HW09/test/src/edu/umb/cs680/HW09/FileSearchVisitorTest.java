ba79a291c23dda0453961ea92b434a23f090e95f
ashil30 <ashil.shah30@gmail.com>
2022-12-19 23:59:51 -0500

    updated 09

--- HW09/test/src/edu/umb/cs680/HW09/FileSearchVisitorTest.java
+++ HW09/test/src/edu/umb/cs680/HW09/FileSearchVisitorTest.java
@@ -14,32 +14,32 @@ import edu.umb.cs680.HW09.fs.util.FileSearchVisitor;
 
 public class FileSearchVisitorTest {
 	private static Directory root;
-	private static Directory src;
+	private static Directory apps;
 	private static Directory bin;
 	private static Directory home;
-	private static Directory test;
+	private static Directory pictures;
 	private static File x;
 	private static File y;
 	private static File a;
 	private static File b;
 	private static File c;
-	private static Link m;
-	private static Link n;
+	private static Link e;
+	private static Link d;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		root = new Directory(null, "root", 0, LocalDateTime.now());
-		src = new Directory(root, "src", 0, LocalDateTime.now());
+		apps = new Directory(root, "apps", 0, LocalDateTime.now());
 		bin = new Directory(root, "bin", 0, LocalDateTime.now());
 		home = new Directory(root, "home", 0, LocalDateTime.now());
-		test = new Directory(home, "test", 0, LocalDateTime.now());
-		x = new File(src, "x", 10, LocalDateTime.now());
+		pictures = new Directory(home, "pictures", 0, LocalDateTime.now());
+		x = new File(apps, "x", 10, LocalDateTime.now());
 		y = new File(bin, "y", 20, LocalDateTime.now());
-		a = new File(test, "a", 30, LocalDateTime.now());
-		b = new File(test, "b", 40, LocalDateTime.now());
+		a = new File(pictures, "a", 30, LocalDateTime.now());
+		b = new File(pictures, "b", 40, LocalDateTime.now());
 		c = new File(home, "c", 50, LocalDateTime.now());
-		m = new Link(home, "m", 0, LocalDateTime.now(), bin);
-		n = new Link(test, "n", 0, LocalDateTime.now(), y);
+		e = new Link(apps, "e", 0, LocalDateTime.now(), x);
+		d = new Link(pictures, "d", 0, LocalDateTime.now(), pictures);
 
 	}
 
